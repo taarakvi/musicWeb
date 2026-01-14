bc73a5ab40b193c7e8b0322da9882351a8bcea84
taarakvi <taarakvi777@gmail.com>
2026-01-15 00:42:42 +0530

    musicWeb

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ buttun.addEventListener("click",()=>{
         buttun.textContent = "play"
     }
 })
+
+// heder section
+ function barSect(){
+    let barOpt = document.querySelector(".barOpt")
+    let barIcon = document.querySelector(".barIcon")
+    barIcon.addEventListener("click",()=>{
+        barOpt.classList.toggle("barOptions")  
+        console.log("clicked bar")
+    })
+   document.querySelector(".Maincontainer").addEventListener("click",()=>{
+    barOpt.classList.remove("barOptions") 
+   })
+ }barSect()
+
+ function cardSect(){
+    let cards = document.querySelectorAll(".card")
+    cards.forEach(element =>{
+        console.log(element.getHTML())
+    })
+ }cardSect()
